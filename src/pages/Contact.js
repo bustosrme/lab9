7e082b76ee34a70f5e8f3b76a94ccab5543d2f45
Mauro Bustos <bustosrme@gmail.com>
2022-04-24 13:11:59 -0300

    Creada pagina de contacto

---
+++ src/pages/Contact.js
@@ -0,0 +1,66 @@
+import { FaFacebookSquare, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import React from "react";
+import { Form, Field } from 'react-final-form';
+
+const onSubmit = async values => {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    await sleep(300);
+    window.alert(JSON.stringify(values, 0, 2));
+};
+
+function Contact() {
+    return (
+        <div>
+            <Navbar fondo="bg-blue-700" />
+            <div className='lg:flex justify-center  mt-32 lg:mt-44 center'>
+                <div className='lg:w-7/12 ml-8 lg:px-28 text-lg text-gray-600 font-serif'>
+                    <h1 className='text-blue-700 text-5xl'>Ponte en contacto con Lab9, queremos conocerte.</h1>
+                    <p className='pt-2'>¿Tienes una idea extraordinaria, un desafío que te mantiene despierto por las noches?</p>
+                    <p className='py-10'>¿Quieres que hablemos sobre los últimos desarrollos tecnológicos y digitales, que podrías aplicar en tu empresa?</p>
+                    <p>Siéntete libre para plantearnos estas u otras inquietudes, contáctanos.</p>
+                    <p className='text-blue-700 pt-8'>Dirección</p>
+                    <p>San Miguel de Tucumán (4000) Tucumán, Argentina</p>
+                    <p className='text-blue-700 pt-6'>Teléfono</p>
+                    <p className='font-mono'>+54 381 3174279/+54 381 5512810</p>
+                    <p className='text-blue-700 pt-6'>Correo electrónico</p>
+                    <p>info@lab9.co</p>
+                    <div className='text-blue-700 mt-10 grid grid-flow-col pr-80'>
+                        <a href='https://www.facebook.com/lab9.digital.agency' target={'_blank'}><FaFacebookSquare size={30} /></a>
+                        <a href='https://www.instagram.com/lab9.agencia/' target={'_blank'}><FaInstagram size={30} /></a>
+                        <a href='https://www.linkedin.com/company/lab-9-agencia-digital' target={'_blank'}><FaLinkedin size={30} /></a>
+                        <a href='https://api.whatsapp.com/send?phone=5493815512810&text=Hola,%20me%20interesa%20comunicarme%20con%20Lab9' target={'_blank'}><FaWhatsapp size={30} /></a>
+                    </div>
+                </div>
+                <div className='h-screen lg:w-5/12 right-0 rounded-2xl text-black'>
+                    <form className='p-4' onSubmit={onSubmit}>
+                        <div>
+                            <label className='block pt-6'>Nombre<span className="obligatorio">*</span></label>
+                            <input className='input input-bordered input-primary w-full max-w-xs bg-gray-100 border-white' type="text" name="introducir_nombre" id="nombre" required="obligatorio" placeholder="Nombre" />
+                        </div>
+                        <div>
+                            <label className='block pt-6'>Email<span className="obligatorio">*</span></label>
+                            <input className='input input-bordered input-primary w-full max-w-xs bg-gray-100 border-white' type="email" name="introducir_email" id="email" required="obligatorio" placeholder="Correo Electrónico" />
+                        </div>
+                        <div>
+                            <label className='block pt-6'>Asunto<span className="obligatorio">*</span></label>
+                            <input className='input input-bordered input-primary w-full max-w-xs bg-gray-100 border-white' type="text" name="introducir_asunto" id="assunto" required="obligatorio" placeholder="Asunto" />
+                        </div>
+                        <div>
+                            <label className='block pt-6'>Mensaje<span className="obligatorio">*</span></label>
+                            <input className='input input-bordered input-primary w-full max-w-xs bg-gray-100 border-white' name="introducir_mensaje" id="mensaje" required="obligatorio" placeholder="Deja aquí tu mensaje..." />
+                        </div>
+                        <p className="pt-4 pb-10 text-sm">
+                            <span className="obligatorio"> * </span>los campos son obligatorios.
+                        </p>
+                        <button className='btn btn-primary btn-lg' type="submit" name="enviar_formulario" id="enviar"><p>Enviar</p></button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Contact;
