0642601b42f01ea51286f4f24c2c6440ac97f136
Mauro Bustos <bustosrme@gmail.com>
2022-04-26 23:17:51 -0300

    Importacion de fuentes Comfortaa y Chivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'chivo' : ['Chivo', 'sans-serif'],
+        'comfortaa' : ['Comfortaa', 'cursive']
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
